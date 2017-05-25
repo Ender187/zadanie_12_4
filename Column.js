709b7f3f0d865d650fc2e0da22a47d8782291fcb
Ender187 <m.hiltawski@hotmail.com>
2017-05-25 16:56:40 +0200

    ostylowanie strony, poprawa kodu column.js

--- Column.js
+++ Column.js
@@ -1,8 +1,9 @@
 function Column(id, name) {
 	var self = this;
+	
 	this.id = id;
-	this.name = name || 'No name given';
-}
+	this.name = name || "Nie podano nazwy";
+	this.element = createColumn();
 
 	function createColumn() {
 		// TWORZENIE NOWYCH WĘZŁÓW
@@ -10,7 +11,7 @@ function Column(id, name) {
 		var columnTitle = $('<h2 class="column-title">' + self.name + '</h2>');
 		var columnCardList = $('<ul class="card-list"></ul>');
 		var columnDelete = $('<button class="btn-delete">x</button>');
-		var columnAddCard = $('<button class="column-add-card">Dodaj kartę</button>');
+		var columnAddCard = $('<button class="btn btn-primary btn-sm">Dodaj kartę</button>');
 		
 		// PODPINANIE ODPOWIEDNICH ZDARZEŃ POD WĘZŁY
 		columnDelete.click(function() {
@@ -18,20 +19,21 @@ function Column(id, name) {
 		});
 		
 		columnAddCard.click(function(event) {
-			var cardName = prompt("Enter the name of the card");
+			var cardName = prompt("Wpisz nazwę karty");
 			event.preventDefault();
-	$.ajax({
-    	url: baseUrl + '/card',
-    	method: 'POST',
-    	data: {
-    	name: cardName,
-    	bootcamp_kanban_column_id: self.id
-    },
-    success: function(response) {
-        var card = new Card(response.id, cardName);
-        self.createCard(card);
-    }
-});
+			$.ajax({
+				url: baseUrl + '/card',
+				method: 'POST',
+				data: {
+					name: cardName,
+					bootcamp_kanban_column_id: self.id
+					},
+				success: function(response) {
+					var card = new Card(response.id, cardName);
+					self.createCard(card);
+				}
+			});
+		});
 			
 			// KONSTRUOWANIE ELEMENTU KOLUMNY
 		column.append(columnTitle)
@@ -39,19 +41,21 @@ function Column(id, name) {
 			.append(columnAddCard)
 			.append(columnCardList);
 			return column;
-	
+		}
+	}
 Column.prototype = {
 	createCard: function(card) {
 	  this.element.children('ul').append(card.element);
 	},
 	deleteColumn: function() {
-    var self = this;
-    $.ajax({
-      url: baseUrl + '/column/' + self.id,
-      method: 'DELETE',
-      success: function(response){
-        self.element.remove();
-      }
-    });
- }
-}
+		var self = this;
+		$.ajax({
+			url: baseUrl + '/column/' + self.id,
+			method: 'DELETE',
+			success: function(response){
+			self.element.remove();
+			}
+		});
+	}
+};
+
